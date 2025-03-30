49e1d4a65e600636fa78467f857aa8f4ed1b86c3
timsun <t_nurlanov@kbtu.kz>
2025-03-30 20:18:47 +0500

    Lab8

---
+++ lab8/shop_back/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:pk>/', views.ProductDetail.as_view()),
+    path('categories/', views.CategoryList.as_view()),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view()),
+    path('categories/<int:id>/products/', views.ProductsByCategory.as_view()),
+]
